7edcac52f27bf8bdd1a8e0b273bc92309d738f90
Saad Ali Khan <saad.khan@din.global>
2026-02-04 17:43:38 +0500

    chore: project setup

---
+++ src/assets/icons/index.js
@@ -0,0 +1,7 @@
+import ButterIcon from "./Butter.svg";
+import FlourIcon from "./Flour.svg";
+import MilkIcon from "./Milk.svg";
+import YogurtIcon from "./Yogurt.svg";
+import GheeIcon from "./Ghee.svg";
+
+export { ButterIcon, FlourIcon, MilkIcon, YogurtIcon, GheeIcon };
