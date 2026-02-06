58447c89fefbe8cff0948143d1bb6937c8b12f6a
Saad Ali Khan <saad.khan@din.global>
2026-02-07 01:44:50 +0500

    chore: deployment done

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,5 +3,19 @@ import FlourIcon from "./Flour.svg";
 import MilkIcon from "./Milk.svg";
 import YogurtIcon from "./Yogurt.svg";
 import GheeIcon from "./Ghee.svg";
+import FarmerImpowermentIcon from "./Farmers_Empowerment.svg";
+import DistributorIcon from "./Distributors.svg";
+import InstitutionalBuyersIcon from "./Institutional_Buyers.svg";
+import LocationOnIcon from "./location_on.svg";
+import PartnersIcon from "./Partner.svg";
+import ProductsIcon from "./Products.svg";
+import RetailStoresIcon from "./Retailers_store.svg";
+import SuperMarketIcon from "./Supermarket.svg";
+import StoreIcon from "./store.svg";
+import StoreOrangeIcon from "./store_orange.svg";
+import RewardIcon from "./Reward.svg";
+import RetailersIcon from "./Retailers.svg";
 
-export { ButterIcon, FlourIcon, MilkIcon, YogurtIcon, GheeIcon };
+
+
+export { ButterIcon, FlourIcon, MilkIcon, YogurtIcon, GheeIcon, FarmerImpowermentIcon, DistributorIcon, InstitutionalBuyersIcon, LocationOnIcon, PartnersIcon, ProductsIcon, RetailStoresIcon, SuperMarketIcon, StoreIcon, StoreOrangeIcon, RewardIcon, RetailersIcon };
